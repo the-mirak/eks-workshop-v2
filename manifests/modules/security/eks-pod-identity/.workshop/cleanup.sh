74ccc251c3323aff06159148634d2e7b201f370b
The Mirak <k.benzidane@gmail.com>
2024-03-04 17:33:04 +0100

    Update cleanup.sh

--- manifests/modules/security/eks-pod-identity/.workshop/cleanup.sh
+++ manifests/modules/security/eks-pod-identity/.workshop/cleanup.sh
@@ -4,7 +4,7 @@ set -e
 
 POD_ASSOCIATION_ID=$(aws eks list-pod-identity-associations --region $AWS_REGION --cluster-name $EKS_CLUSTER_NAME --service-account carts --namespace carts --output text --query 'associations[0].associationId')
 
-if [ ! "$POD_ASSOCIATION_ID"="None" ]; then
+if [ "$POD_ASSOCIATION_ID"!="None" ]; then
   logmessage "Deleting EKS Pod Identity Association..."
   
   aws eks delete-pod-identity-association --region $AWS_REGION --association-id $POD_ASSOCIATION_ID --cluster-name $EKS_CLUSTER_NAME
@@ -13,7 +13,7 @@ fi
 
 check=$(aws eks list-addons --cluster-name $EKS_CLUSTER_NAME --region $AWS_REGION --query "addons[? @ == 'eks-pod-identity-agent']" --output text)
 
-if [ ! "$check"="None" ]; then
+if [ "$check"!="None" ]; then
   logmessage "Deleting EKS Pod Identity Agent addon..."
 
   aws eks delete-addon --cluster-name $EKS_CLUSTER_NAME --addon-name eks-pod-identity-agent --region $AWS_REGION
